a380087e481184a9089e643fd5dc539efc4984c3
David Delatorre <99780693+David-Delatorre@users.noreply.github.com>
2025-02-11 21:29:52 -0800

    Add files via upload

---
+++ FileCopy.c
@@ -0,0 +1,18 @@
+/*
+ * FileCopy.c
+ * 
+ * This program opens a file and writes its contents to a pipe 
+ */
+ 
+#include <stdio.h>
+#include <stdlib.h>
+
+#define BUFFER_SIZE 25
+#define STRING_SIZE 25
+
+int main(int argc, char *argv[])
+{
+   printf("Hello, Visual Studio!\n");
+   return 0;
+               
+}
